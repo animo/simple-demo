cbd29d1a1885d25fe60d384666381ee72fe30319
Jan <jan@animo.id>
2021-09-17 15:02:24 +0200

    fix

--- src/api/AgentApi.tsx
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosResponse } from "axios";
-
-const baseUrl = process.env.REACT_APP_HOST_BACKEND ?? "http://localhost:49160";
-
-const api = axios.create({ baseURL: baseUrl });
-
-export const getAgentInfo = (): Promise<AxiosResponse> => {
-  return api.get("/agent/");
-};
