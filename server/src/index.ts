2c7ac84e7e426d28d8c97451c0b67a2ac32c67e7
Jan <jan@animo.id>
2021-10-04 16:24:22 +0200

    fix: removed ngrok
    Signed-off-by: Jan <jan@animo.id>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,23 @@
 import { Agent, AutoAcceptCredential, HttpOutboundTransport, InitConfig, LogLevel } from "@aries-framework/core";
-import { connect } from "ngrok";
 import { BCOVRIN_TEST_GENESIS } from "./utils";
 import { agentDependencies, HttpInboundTransport } from "@aries-framework/node";
 import { startServer } from "@aries-framework/rest";
 import { TestLogger } from "./logger";
 
+const endpoint = process.env.AGENT_ENDPOINT ?? "http://localhost:5001";
+const port = process.env.AGENT_PORT ?? 5001;
+
 const run = async () => {
   const logger = new TestLogger(LogLevel.debug);
-
-  const endpoint = await connect(5001);
   const agentConfig: InitConfig = {
-    label: "Animo",
+    label: "Animo Demo Agent",
     walletConfig: {
-      id: "Animo",
-      key: "Animo",
+      id: "Animo Demo Agent",
+      key: "Animo Demo Agent",
     },
     poolName: "pool-holder",
     genesisTransactions: BCOVRIN_TEST_GENESIS,
-    publicDidSeed: "testtesttesttesttesttesttesttest",
+    publicDidSeed: "4bd9c0200f4ba2ad8069d8dcea1c9ea9",
     logger: logger,
     endpoints: [endpoint],
     autoAcceptConnections: true,
@@ -28,7 +28,7 @@ const run = async () => {
   const agent = new Agent(agentConfig, agentDependencies);
 
   const httpInbound = new HttpInboundTransport({
-    port: 5001,
+    port: Number(port),
   });
 
   agent.registerInboundTransport(httpInbound);
