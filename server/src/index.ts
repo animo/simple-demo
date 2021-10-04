262850a1ab0ff172c5c379770f50c0fa950d4a06
Jan <jan@animo.id>
2021-10-04 10:48:40 +0200

    feat: added logger for debugging
    Signed-off-by: Jan <jan@animo.id>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
-import { Agent, AutoAcceptCredential, HttpOutboundTransport, InitConfig } from "@aries-framework/core";
+import { Agent, AutoAcceptCredential, HttpOutboundTransport, InitConfig, LogLevel } from "@aries-framework/core";
 import { connect } from "ngrok";
 import { BCOVRIN_TEST_GENESIS } from "./utils";
 import { agentDependencies, HttpInboundTransport } from "@aries-framework/node";
 import { startServer } from "@aries-framework/rest";
+import { TestLogger } from "./logger";
 
 const run = async () => {
+  const logger = new TestLogger(LogLevel.debug);
+
   const endpoint = await connect(3001);
   const agentConfig: InitConfig = {
     label: "Animo",
@@ -15,6 +18,7 @@ const run = async () => {
     poolName: "pool-holder",
     genesisTransactions: BCOVRIN_TEST_GENESIS,
     publicDidSeed: "testtesttesttesttesttesttesttest",
+    logger: logger,
     endpoints: [endpoint],
     autoAcceptConnections: true,
     autoAcceptCredentials: AutoAcceptCredential.ContentApproved,
